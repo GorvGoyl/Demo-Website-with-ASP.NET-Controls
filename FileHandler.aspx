aef424df227be6fd98af45e2d7f0d7f513fdc6dd
jerrygoyal <1gouravgg@gmail.com>
2015-08-12 15:43:25 +0530

    added file uploading control

---
+++ FileHandler.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FileHandler.aspx.cs" Inherits="FileHandler" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+    <div>
+    
+    </div>
+    <table class="style1">
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+    
+        <asp:FileUpload ID="FileUpload1" runat="server" Width="342px" />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload File" 
+                    onclick="btnUpload_Click" />
+    
+            </td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+    <asp:Label ID="lblMessage" runat="server" Font-Bold="true"></asp:Label>
+            </td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+    </table>
+ 
+    </form>
+</body>
+</html>
---
+++ FileHandler.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class FileHandler : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            // Get the file extension
+            string fileExtension = System.IO.Path.GetExtension(FileUpload1.FileName);
+
+            if (fileExtension.ToLower() != ".doc" && fileExtension.ToLower() != ".docx" && fileExtension.ToLower() != ".pdf")
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Only files with .doc or.docx or .pdf extension are allowed";
+            }
+            else
+            {
+                // Get the file size
+                int fileSize = FileUpload1.PostedFile.ContentLength;
+                // If file size is greater than 2 MB
+                if (fileSize > 2097152)
+                {
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                    lblMessage.Text = "File size cannot be greater than 2 MB";
+                }
+                else
+                {
+                    // Upload the file
+                    FileUpload1.SaveAs(Server.MapPath("~/Uploads/" + FileUpload1.FileName));
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    lblMessage.Text = "File uploaded successfully";
+                }
+            }
+        }
+        else
+        {
+            lblMessage.ForeColor = System.Drawing.Color.Red;
+            lblMessage.Text = "Please select a file (Max. size is 2MB)";
+        } 
+    }
+}
